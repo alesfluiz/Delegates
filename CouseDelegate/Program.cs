a148e2f9eb089b61c5f03afd8d5743392f3e90b7
unknown <alesfluiz@gmail.com>
2023-05-21 16:39:47 -0300

    first commit

---
+++ CouseDelegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using CourseDelegate.Service;
+using System;
+
+namespace CourseDelegate
+{
+    internal class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Max;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
