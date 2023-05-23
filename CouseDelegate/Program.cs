4a7c0dbb6bc5a3f340de14a803ce4868263f203d
unknown <alesfluiz@gmail.com>
2023-05-22 23:21:58 -0300

    Action

--- CouseDelegate/Program.cs
+++ CouseDelegate/Program.cs
@@ -1,20 +1,30 @@
-﻿using CourseDelegate.Service;
+﻿using CourseDelegate.Entities;
 using System;
 
 namespace CourseDelegate
 {
     internal class Program
     {
-        delegate double BinaryNumericOperation(double n1, double n2);
         static void Main(string[] args)
         {
-            double a = 10;
-            double b = 12;
+            List<Product> list = new List<Product>();
 
-            BinaryNumericOperation op = CalculationService.Max;
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.90));
 
-            double result = op(a, b);
-            Console.WriteLine(result);
+            list.ForEach(UpdatePrice);
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
         }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }    
+
     }
 }
